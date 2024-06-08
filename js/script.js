286f9ed94634b9267d0d7ee0ed11a63b7c0524e2
Reynhard Powiwi <90497513+reypowgithub@users.noreply.github.com>
2024-06-08 23:41:49 +0700

    update
    - memperbaiki member dan navbar
    - update warna tabel
    - membuat index_2 untuk menggabungkan landing page dan tabel

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-allSideMenu.forEach((item) => {
-  const li = item.parentElement;
+// allSideMenu.forEach((item) => {
+//   const li = item.parentElement;
 
-  item.addEventListener("click", function () {
-    allSideMenu.forEach((i) => {
-      i.parentElement.classList.remove("active");
-    });
-    li.classList.add("active");
-  });
-});
+//   item.addEventListener("click", function () {
+//     allSideMenu.forEach((i) => {
+//       i.parentElement.classList.remove("active");
+//     });
+//     li.classList.add("active");
+//   });
+// });
 
 // // TOGGLE SIDEBAR
 // const menuBar = document.querySelector('#content nav .bx.bx-menu');
@@ -17,76 +17,158 @@ allSideMenu.forEach((item) => {
 // 	sidebar.classList.toggle('hide');
 // })
 
-const burger = document.querySelector(".burger");
-const navbar = document.querySelector(".navbar");
+// const searchButton = document.querySelector(
+//   "#content nav form .form-input button"
+// );
+// const searchButtonIcon = document.querySelector(
+//   "#content nav form .form-input button .bx"
+// );
+// const searchForm = document.querySelector("#content nav form");
 
-burger.addEventListener("click", () => {
-  navbar.classList.toggle("nav-active");
-  burger.classList.toggle("toggle");
-});
+// searchButton.addEventListener("click", function (e) {
+//   if (window.innerWidth < 576) {
+//     e.preventDefault();
+//     searchForm.classList.toggle("show");
+//     if (searchForm.classList.contains("show")) {
+//       searchButtonIcon.classList.replace("bx-search", "bx-x");
+//     } else {
+//       searchButtonIcon.classList.replace("bx-x", "bx-search");
+//     }
+//   }
+// });
 
-const searchButton = document.querySelector(
-  "#content nav form .form-input button"
-);
-const searchButtonIcon = document.querySelector(
-  "#content nav form .form-input button .bx"
-);
-const searchForm = document.querySelector("#content nav form");
+// if (window.innerWidth < 768) {
+//   sidebar.classList.add("hide");
+// } else if (window.innerWidth > 576) {
+//   searchButtonIcon.classList.replace("bx-x", "bx-search");
+//   searchForm.classList.remove("show");
+// }
 
-searchButton.addEventListener("click", function (e) {
-  if (window.innerWidth < 576) {
-    e.preventDefault();
-    searchForm.classList.toggle("show");
-    if (searchForm.classList.contains("show")) {
-      searchButtonIcon.classList.replace("bx-search", "bx-x");
-    } else {
-      searchButtonIcon.classList.replace("bx-x", "bx-search");
-    }
-  }
-});
-
-if (window.innerWidth < 768) {
-  sidebar.classList.add("hide");
-} else if (window.innerWidth > 576) {
-  searchButtonIcon.classList.replace("bx-x", "bx-search");
-  searchForm.classList.remove("show");
-}
-
-window.addEventListener("resize", function () {
-  if (this.innerWidth > 576) {
-    searchButtonIcon.classList.replace("bx-x", "bx-search");
-    searchForm.classList.remove("show");
-  }
-});
+// window.addEventListener("resize", function () {
+//   if (this.innerWidth > 576) {
+//     searchButtonIcon.classList.replace("bx-x", "bx-search");
+//     searchForm.classList.remove("show");
+//   }
+// });
 
 // Ditambah Dari sini data CHART
 // Fungsi untuk memuat data dari file JSON
 
-const swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
-  spaceBetween: 25,
-  loop: true,
-  centerSlide: "true",
-  fade: "true",
-  grabCursor: "true",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    950: {
-      slidesPerView: 3,
-    },
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  const wrapper = document.querySelector(".wrapper");
+  const carousel = document.querySelector(".carousel");
+  const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+  const arrowBtns = document.querySelectorAll(".wrapper i");
+  const carouselChildrens = [...carousel.children];
+  const burger = document.querySelector(".burger");
+  const navbar = document.querySelector(".navbar");
+
+  burger.addEventListener("click", () => {
+    navbar.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+  });
+
+  let isDragging = false,
+    isAutoPlay = true,
+    startX,
+    startScrollLeft,
+    timeoutId;
+
+  // Get the number of cards that can fit in the carousel at once
+  let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+  // Insert copies of the last few cards to beginning of carousel for infinite scrolling
+  carouselChildrens
+    .slice(-cardPerView)
+    .reverse()
+    .forEach((card) => {
+      carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+    });
+
+  // Insert copies of the first few cards to end of carousel for infinite scrolling
+  carouselChildrens.slice(0, cardPerView).forEach((card) => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+  });
+
+  // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+  carousel.classList.add("no-transition");
+  carousel.scrollLeft = carousel.offsetWidth;
+  carousel.classList.remove("no-transition");
+
+  // Add event listeners for the arrow buttons to scroll the carousel left and right
+  arrowBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      carousel.scrollLeft +=
+        btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
+  });
+
+  const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    // Records the initial cursor and scroll position of the carousel
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+  };
+
+  const dragging = (e) => {
+    if (!isDragging) return; // if isDragging is false return from here
+    // Updates the scroll position of the carousel based on the cursor movement
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+  };
+
+  const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+  };
+
+  const infiniteScroll = () => {
+    // If the carousel is at the beginning, scroll to the end
+    if (carousel.scrollLeft === 0) {
+      carousel.classList.add("no-transition");
+      carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+      carousel.classList.remove("no-transition");
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if (
+      Math.ceil(carousel.scrollLeft) ===
+      carousel.scrollWidth - carousel.offsetWidth
+    ) {
+      carousel.classList.add("no-transition");
+      carousel.scrollLeft = carousel.offsetWidth;
+      carousel.classList.remove("no-transition");
+    }
+
+    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+    clearTimeout(timeoutId);
+    if (!wrapper.matches(":hover")) autoPlay();
+  };
+
+  const autoPlay = () => {
+    if (window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+    // Autoplay the carousel after every 2500 ms
+    timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 2500);
+  };
+  autoPlay();
+
+  carousel.addEventListener("mousedown", dragStart);
+  carousel.addEventListener("mousemove", dragging);
+  document.addEventListener("mouseup", dragStop);
+  carousel.addEventListener("scroll", infiniteScroll);
+  wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+  wrapper.addEventListener("mouseleave", autoPlay);
+
+  const dataLink = document.getElementById("data-link");
+  const tableSection = document.getElementById("table");
+
+  dataLink.addEventListener("click", function (event) {
+    event.preventDefault(); // Mencegah perilaku default dari link
+    // Menampilkan bagian tabel dan menyembunyikan bagian lainnya
+    tableSection.style.display = "block";
+    document
+      .querySelectorAll("main > section:not(#table)")
+      .forEach(function (section) {
+        section.style.display = "none";
+      });
+  });
 });
